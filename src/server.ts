35d1cebf9241390507d7fc9b1c71a3f22807886d
PedroSbardelotto <sbardelottopedro97@gmail.com>
2025-03-28 14:31:59 -0300

    resolvendo bug do servidor listen

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,12 @@
 import dotenv from "dotenv";
 import express from "express";
-import app from "./app";
+import app from "./app"; // Importa o app configurado
+import { products } from "./models/productModel";
 
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
